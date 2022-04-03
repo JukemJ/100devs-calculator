69617bffffb41bb919ba4e545130385feda6d37a
JukemJ <chris@dixn.dev>
2022-04-03 17:43:38 -0400

    created basic calculator shape, can type numbers in to screen

---
+++ script.js
@@ -0,0 +1,33 @@
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const zero = document.querySelector('#zero')
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const multiply = document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+const equals = document.querySelector('#equals')
+const decimal = document.querySelector('#decimal')
+const screen = document.querySelector('#screen')
+
+firstNum = []
+secondNum = []
+solution = []
+
+one.addEventListener("click", function(){screen.innerText += '1'})
+two.addEventListener("click", function(){screen.innerText += '2'})
+three.addEventListener("click", function(){screen.innerText += '3'})
+four.addEventListener("click", function(){screen.innerText += '4'})
+five.addEventListener("click", function(){screen.innerText += '5'})
+six.addEventListener("click", function(){screen.innerText += '6'})
+seven.addEventListener("click", function(){screen.innerText += '7'})
+eight.addEventListener("click", function(){screen.innerText += '8'})
+nine.addEventListener("click", function(){screen.innerText += '9'})
+zero.addEventListener("click", function(){screen.innerText += '0'})
+
