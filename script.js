c6b46971bf711835aa6bcf6d839f8b542e9f23b1
JukemJ <93483466+JukemJ@users.noreply.github.com>
2022-04-03 20:25:57 -0400

    operations working

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ const equals = document.querySelector('#equals')
 const decimal = document.querySelector('#decimal')
 const screen = document.querySelector('#screen')
 
-firstNum = []
-secondNum = []
-solution = []
+workingNum = null
+lastOperation = ''
 
 one.addEventListener("click", function(){screen.innerText += '1'})
 two.addEventListener("click", function(){screen.innerText += '2'})
@@ -29,5 +28,65 @@ six.addEventListener("click", function(){screen.innerText += '6'})
 seven.addEventListener("click", function(){screen.innerText += '7'})
 eight.addEventListener("click", function(){screen.innerText += '8'})
 nine.addEventListener("click", function(){screen.innerText += '9'})
-zero.addEventListener("click", function(){screen.innerText += '0'})
-
+plus.addEventListener("click", function(){
+    if (workingNum == null){
+        workingNum = Number(screen.innerText)
+        screen.innerText = ''
+    }
+    else {
+        workingNum += Number(screen.innerText)
+        screen.innerText = ''
+    }
+    lastOperation = 'plus'
+})
+minus.addEventListener("click", function(){
+    if (workingNum == null){
+        workingNum = Number(screen.innerText)
+        screen.innerText = ''
+    }
+    else {
+        workingNum -= Number(screen.innerText)
+        screen.innerText = ''
+    }
+    lastOperation = 'minus'
+})
+multiply.addEventListener("click", function(){
+    if (workingNum == null){
+        workingNum = Number(screen.innerText)
+        screen.innerText = ''
+    }
+    else {
+        workingNum *= Number(screen.innerText)
+        screen.innerText = ''
+    }
+    lastOperation = 'multiply'
+})
+divide.addEventListener("click", function(){
+    if (workingNum == null){
+        workingNum = Number(screen.innerText)
+        screen.innerText = ''
+    }
+    else {
+        workingNum /= Number(screen.innerText)
+        screen.innerText = ''
+    }
+    lastOperation = 'divide'
+})
+equals.addEventListener('click', function(){
+    if (lastOperation == 'plus') {
+        workingNum += Number(screen.innerText)
+        screen.innerText = workingNum
+    }
+    if (lastOperation == 'minus'){
+        workingNum -= Number(screen.innerText)
+        screen.innerText = workingNum
+    }
+    if (lastOperation == 'multiply') {
+        workingNum *= Number(screen.innerText)
+        screen.innerText = workingNum
+    }
+    if (lastOperation == 'divide') {
+        workingNum /= Number(screen.innerText)
+        screen.innerText = workingNum
+    }
+})
